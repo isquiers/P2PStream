7638bef8cb4ac809b08a92c15d112e36413486f1
sean cork <seantcork@gmaol.com>
2019-04-18 16:10:06 -0400

    java rmi

--- streamer.java
+++ streamer.java
@@ -3,8 +3,35 @@
 *
 */
 
-public class Streamer{
+public class Node {
 
+  //What I think we need to do.
+
+  /* 1. What data structure to store the original file/how to split
+  *
+  * 2. Data structure for timestamp, data
+  *
+  * 3. Structure for node list
+  *
+  * 4. Sean's Idea is chain of kind of linked list
+  *
+  * 5. rolling cache of significant enough size
+  *
+  * 6. cache of master index?
+  *
+  * 7.
+  * methods (all)
+  * - join ( viewer )
+  * - leave ( viewer )
+  * - request Data ( viewer )
+  * - send Data/respond to request ( everyone )
+  * - update master ( viewer )
+  * - update index ( master )
+  * - create network ( master )
+  * - update index cache ( viewer )
+  * - request cache
+  *
+  */
 
 
 }
