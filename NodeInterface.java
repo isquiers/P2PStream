1439cf1b3c272a0448ab212663a4dea122a0d0a9
sean cork <seantcork@gmaol.com>
2019-04-19 14:29:29 -0400

    Update NodeInterface.java

--- NodeInterface.java
+++ NodeInterface.java
@@ -5,18 +5,15 @@ public interface Node extends Remote{
 
 // public void leave()
 
+
 public DataBlock requestData(String feederIP) throws RemoteException;
 
 public void sendData(Object dataBlock) throws RemoteException;
 
 public String updateMaster(String masterIP, String deadNodeIP) throws RemoteException;
 
-// public createNetwork()
+public String getMasterIndex(String masterIp) throws RemoteException;
 
-public updateIndexCache;
-
-public requestCache;
-
-public play;
+public play();
 
 }
