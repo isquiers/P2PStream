6dd681b20046495de3e75716c474be1b4a16b360
Ian Squiers <iansquiers321@gmail.com>
2019-04-19 14:31:53 -0400

    Notes

--- Node.java
+++ Node.java
@@ -1,34 +1,65 @@
+//Questions for Sean about Java RMI.
+//1. Differnt interface for node and master
+//2. questions abut different java files
+//3. remote interface vs interface.
+//4. client server on same Node
+//5. data structure for branching chain
+//6. linked list acceptable
+//7. suggestions on modeling bitrate both from client and for "streaming"
+//8 . client server on different threads or different processes.
+
+package com.technicalkeeda.app;
+import java.net.InetAddress;
+ 
 public class Node {
 
   Integer dataBlockSize;
   Integer cacheSize;
-  ArrayList<Object> Cache;
+  ArrayList<Object> cache;
   String masterIp;
+  String selfIp;
+  String filename;
+  boolean isMaster = false;
 
-  public static void main(String args[]) {
-    // [dataBlockSize][cacheSize][MasterIp][Optional Content filename]
+  public static void main(String[] args) {
+    // [dataBlockSize][cacheSize][MasterIp][*Optional Content filename]
     //
-    int dataBlockSize = Integer.ParseInt(args[0]);
-    int cacheSize = Integer.ParseInt(args[1]);
+    this.dataBlockSize = Integer.ParseInt(args[0]);
+    this.cacheSize = Integer.ParseInt(args[1]);
+    this.masterIp = Integer.ParseInt(args[2]);
+    InetAddress inetAddress = InetAddress.getLocalHost();
+    this.selfIp = inetAddress.getHostAddress();
 
-  }
+    if (args.length == 4) {
+      isMaster = true;
+      filename = Integer.ParseInt(args[3]);
+      Thread thread1(loadCache);
+      // start thread that loadscace
+    } else {
+      Thread thread2(startRequestingData);
+    }
+    Thread thread2(playFromCache);
 
-  public void playFromCache(ArrayList<Object E>) {
-
-  }
-
-
-
-
-  public void listenForReq() {
     try{
-
       NodeInterface stub=new NodeInterfaceRemote();
       Naming.rebind("rmi://localhost:5000/sonoo",stub);
-
     }
       catch(Exception e){System.out.println(e);}
     }
+
+  }
+
+  public void startRequestingData() {
+
+  }
+
+  public void playFromCache() {
+
+  }
+
+  //only for masterIP
+  void loadCache() {
+    // can use this.cache
   }
 
   public setDataBlockSize(int dataBlockSize){
